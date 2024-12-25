3414c1dd7e81098a5104d69fee06be7303dfee08
Raman Sinclair <raman@arsinclair.dev>
2024-12-25 17:50:57 +0100

    refactor: emotion cache workaround

--- src/lib/RootStyleRegistry.tsx
+++ src/lib/RootStyleRegistry.tsx
@@ -3,33 +3,31 @@
 import createCache from "@emotion/cache";
 import { CacheProvider } from "@emotion/react";
 import { useServerInsertedHTML } from "next/navigation";
-import { useState, type JSX } from "react";
+import { useMemo, type PropsWithChildren } from "react";
+
+const getEmotionCacheWithFlush = () => {
+    const cache = createCache({ key: "root-style" });
+    cache.compat = true;
+    const prevInsert = cache.insert;
+    let inserted: string[] = [];
+    cache.insert = (...args) => {
+        const serialized = args[1];
+        if (cache.inserted[serialized.name] === undefined) {
+            inserted.push(serialized.name);
+        }
+        return prevInsert(...args);
+    };
+    const flush = () => {
+        const prevInserted = inserted;
+        inserted = [];
+        return prevInserted;
+    };
+    return { cache, flush };
+};
 
 // https://github.com/emotion-js/emotion/issues/2928#issuecomment-1319747902
-export default function RootStyleRegistry({
-    children,
-}: {
-    children: JSX.Element;
-}) {
-    const [{ cache, flush }] = useState(() => {
-        const cache = createCache({ key: "root-style" });
-        cache.compat = true;
-        const prevInsert = cache.insert;
-        let inserted: string[] = [];
-        cache.insert = (...args) => {
-            const serialized = args[1];
-            if (cache.inserted[serialized.name] === undefined) {
-                inserted.push(serialized.name);
-            }
-            return prevInsert(...args);
-        };
-        const flush = () => {
-            const prevInserted = inserted;
-            inserted = [];
-            return prevInserted;
-        };
-        return { cache, flush };
-    });
+export default function RootStyleRegistry({ children }: PropsWithChildren) {
+    const { cache, flush } = useMemo(getEmotionCacheWithFlush, []);
 
     useServerInsertedHTML(() => {
         const names = flush();

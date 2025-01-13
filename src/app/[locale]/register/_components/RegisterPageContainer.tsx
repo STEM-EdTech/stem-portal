b66380041ce763c599c683ef08e106a80d0ef2af
matveeva <ladaivanna.matveeva@gmail.com>
2025-01-13 21:24:10 +0100

    feat: change overlay for background

--- src/app/[locale]/register/_components/RegisterPageContainer.tsx
+++ src/app/[locale]/register/_components/RegisterPageContainer.tsx
@@ -7,7 +7,7 @@ export const RegisterPageContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    background-image: url('/images/background.png');
+    background-image: url('https://7i2wrzbr0panhthc.public.blob.vercel-storage.com/static/stem-logo-background-NAWgmAojRRHvzK1PWKxlky2J9uy8S4.png');
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
@@ -20,7 +20,7 @@ export const RegisterPageContainer = styled.div`
         left: 0;
         width: 100%;
         height: 100%;
-        background-image: url('/images/overlay.png');
+        background-color: rgba(0, 22, 52, 0.4);
         z-index: 1;
     }
 

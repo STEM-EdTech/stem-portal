2098b614d2e05a82b612529ce2e86084c55d2c96
Raman Sinclair <raman@arsinclair.dev>
2025-06-09 02:38:52 +0300

    feat: implement a Draft chatbox (#75)

--- src/app/[locale]/_components/index.tsx
+++ src/app/[locale]/_components/index.tsx
@@ -1,2 +1,5 @@
 export { Header } from "./Header";
 export { LogoutButton } from "./LogoutButton";
+export { Sidebar } from "./Sidebar";
+export { ChatArea } from "./ChatArea";
+export { ChatBox } from "./ChatBox";
